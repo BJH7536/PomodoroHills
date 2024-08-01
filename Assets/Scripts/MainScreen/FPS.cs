378a9eba337e29c50729a554e54ab34351fa5188
BJH <114412598+BJH7536@users.noreply.github.com>
2024-08-01 19:32:51 +0900

    [CHORE]

--- Assets/Scripts/MainScreen/FPS.cs
+++ Assets/Scripts/MainScreen/FPS.cs
@@ -16,15 +16,33 @@ public class FPS : MonoBehaviour
     {
         Application.targetFrameRate = 120;
         
-        _dropDownOptionList.Add(Application.targetFrameRate.ToString());
-        _dropDownOptionList.Add("30");
-        _dropDownOptionList.Add("60");
-        _dropDownOptionList.Add("80");
-        _dropDownOptionList.Add("120");
-        _dropDownOptionList.Add("144");
+        // _dropDownOptionList.Add(Application.targetFrameRate.ToString());
+        // _dropDownOptionList.Add("30");
+        // _dropDownOptionList.Add("60");
+        // _dropDownOptionList.Add("80");
+        // _dropDownOptionList.Add("120");
+        // _dropDownOptionList.Add("144");
+        //
+        // _dropdown.AddOptions(_dropDownOptionList);
+        
+        _dropdown.ClearOptions();       // 기존 옵션 제거
 
+        Resolution[] resolutions = Screen.resolutions;      // 장치가 지원하는 모든 해상도 가져오기
+        HashSet<string> frameRate = new HashSet<string>();        // 중복 제거를 위해 HashSet 사용 
+        
+        // 각 해상도의 프레임레이트를 HashSet에 추가
+        foreach (var res in resolutions)
+        {
+            frameRate.Add(res.refreshRateRatio.ToString());
+        }
+        
+        // 프레임레이트 옵션을 드롭다운에 추가
+        foreach (string fr in frameRate)
+        {
+            _dropDownOptionList.Add(fr.ToString() + " Hz");
+        }
         _dropdown.AddOptions(_dropDownOptionList);
-
+        
         _dropdown.onValueChanged.AddListener(ChangeFPS);
     }
 
@@ -44,7 +62,7 @@ public class FPS : MonoBehaviour
 
     private void ChangeFPS(int idx)
     {
-        int fps = int.Parse(_dropDownOptionList[idx]);
+        int fps = int.Parse(_dropDownOptionList[idx].Substring(0, _dropDownOptionList[idx].Length - 3));
         Application.targetFrameRate = fps;
     }
 }
